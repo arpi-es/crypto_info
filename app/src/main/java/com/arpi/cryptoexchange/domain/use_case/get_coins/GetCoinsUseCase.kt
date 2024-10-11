9cd9e52392affc6d94973c8e126a4bd1b3492f18
Arpi <rp.eskandary@gmail.com>
2024-10-11 12:17:50 +0400

    feat:add getCoins function returning Flow<PagingData<Coin>>

--- app/src/main/java/com/arpi/cryptoexchange/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/arpi/cryptoexchange/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -1,12 +1,16 @@
 package com.arpi.cryptoexchange.domain.use_case.get_coins
 
 
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.arpi.cryptoexchange.common.Resource
+import com.arpi.cryptoexchange.data.local.CoinEntity
 import com.arpi.cryptoexchange.data.mapper.toCoin
 import com.arpi.cryptoexchange.domain.model.Coin
 import com.arpi.cryptoexchange.domain.repository.CoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
@@ -14,6 +18,15 @@ import javax.inject.Inject
 class GetCoinsUseCase @Inject constructor(
         private val repository: CoinRepository,
 ) {
+
+    suspend fun getCoins(): Flow<PagingData<Coin>> {
+        return repository.getCoins().map { value: PagingData<CoinEntity> ->
+            value.map { coinEntity ->
+                coinEntity.toCoin()
+            }
+        }
+    }
+
 
     operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
 
